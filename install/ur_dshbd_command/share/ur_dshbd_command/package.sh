fbb7d082f66f58049b4612c759d27cb7b5c33965
root <root@DESKTOP-SES7TK8>
2024-07-24 17:01:34 +0900

    clear trash files

--- install/ur_dshbd_command/share/ur_dshbd_command/package.sh
+++ install/ur_dshbd_command/share/ur_dshbd_command/package.sh
@@ -80,7 +80,6 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/ur_dshbd_command/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/ur_dshbd_command/hook/ament_prefix_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/ur_dshbd_command/share/ur_dshbd_command/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
